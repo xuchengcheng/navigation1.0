98db53840f3e68d48d13fc778552d0cc1728c1b9
blueorange <2524578253@qq.com>
2022-11-02 16:33:57 +0800

    第一次提交

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //创建数组存放背景url
-var bgs = new Array('url("images/bg01.jpg")', 'url("images/bg02.jpg")', 'url("images/bg04.jpg")', 'url("images/bg05.jpg")', 'url("images/bg08.jpg")', 'url("images/bg25.jpg")', 'url("images/bg09.jpg")', 'url("images/bg10.jpg")', 'url("images/bg12.jpg")', 'url("images/bg13.jpg")', 'url("images/bg25.jpg")', 'url("images/bg15.jpg")', 'url("images/bg17.jpg")', 'url("images/bg19.jpg")', 'url("images/bg20.jpg")', 'url("images/bg21.jpg")', 'url("images/bg22.jpg")', 'url("images/bg23.jpg")', 'url("images/bg25.jpg")');
+var bgs = new Array('url("images/bg/bg01.jpg")', 'url("images/bg/bg02.jpg")', 'url("images/bg/bg04.jpg")', 'url("images/bg/bg05.jpg")', 'url("images/bg/bg08.jpg")', 'url("images/bg/bg25.jpg")', 'url("images/bg/bg09.jpg")', 'url("images/bg/bg10.jpg")', 'url("images/bg/bg12.jpg")', 'url("images/bg/bg13.jpg")', 'url("images/bg/bg25.jpg")', 'url("images/bg/bg15.jpg")', 'url("images/bg/bg17.jpg")', 'url("images/bg/bg19.jpg")', 'url("images/bg/bg20.jpg")', 'url("images/bg/bg21.jpg")', 'url("images/bg/bg22.jpg")', 'url("images/bg/bg23.jpg")', 'url("images/bg/bg25.jpg")', 'url("images/bg/1.png")', 'url("images/bg/2.png")', 'url("images/bg/3.png")', 'url("images/bg/4.png")', 'url("images/bg/5.png")', 'url("images/bg/6.png")');
 
 //随机插入背景函数
 function changeBg() {
